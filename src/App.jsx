5dc2544f16e63b37a4b70b9718f387ebb5b918fb
anantjoshi87 <antjoshi2006@gmail.com>
2026-03-14 11:00:47 +0530

    Fix import case for Search component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import Search from "./components/search";
+import Search from "./components/Search";
 import Loader from "./components/Loader";
 import MovieCard from "./components/MovieCard";
 import { useDebounce } from "react-use";
